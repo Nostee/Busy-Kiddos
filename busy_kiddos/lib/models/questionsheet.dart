8cc4e0e8b711ccdadf3283723bb995f8a0e93599
pingu1699 <mendozamelissa9999@gmail.com>
2021-07-18 13:41:52 +0800

    add question math

--- busy_kiddos/lib/models/questionsheet.dart
+++ busy_kiddos/lib/models/questionsheet.dart
@@ -1,23 +1,30 @@
-class QuestionSheet
-{
-    dynamic questionsScience = {
-      "How many hands do we have?":["2","5","8","3"],
-      "How many feet do we have?":["5","4","2","3"]
-      };
+class QuestionSheet {
+  dynamic questionsScience = {
+    "How many hands do we have?": ["2", "5", "8", "3"],
+    "How many feet do we have?": ["5", "4", "2", "3"]
+  };
 
-    dynamic answersScience = ["2","2"];
+  dynamic answersScience = ["2", "2"];
 
-    dynamic questionsMath = {
-      "How many hands do we have?":["2","5","8","3"],
-      "How many feet do we have?":["5","4","2","3"]
-      };
+  dynamic questionsMath = {
+    "What is 2 x 4?": ["6", "12", "7", "8"],
+    "What is 8 + 5?": ["5", "10", "13", "9"],
+    "What is 6 ÷ 6?": ["15", "1", "12", "45"],
+    "What is 20 - 9?": ["18", "8", "10", "11"],
+    "What is 10 + 6?": ["16", "8", "12", "45"],
+    "What is 9 ÷ 3?": ["15", "8", "3", "45"],
+    "What is 5 - 2?": ["1", "2", "3", "4"],
+    "What is 20 + 2?": ["18", "6", "22", "25"],
+    "What is 1 + 1?": ["2", "4", "6", "8"],
+    "What is 3 x 4?": ["15", "8", "12", "45"],
+  };
 
-    dynamic answersMath = ["2","2"];
+  dynamic answersMath = ["8", "13", "1", "11", "16", "3", "3", "22", "2", "12"];
 
-    dynamic questionsEnglish = {
-      "How many hands do we have?":["2","5","8","3"],
-      "How many feet do we have?":["5","4","2","3"]
-      };
+  dynamic questionsEnglish = {
+    "How many hands do we have?": ["2", "5", "8", "3"],
+    "How many feet do we have?": ["5", "4", "2", "3"]
+  };
 
-    dynamic answersEnglish = ["2","2"];
+  dynamic answersEnglish = ["2", "2"];
 }
